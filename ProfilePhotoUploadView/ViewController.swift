f759cdd1cab4bbd8337020da2a362e2e3e915835
Woohyun David Lee <cumgongchoong@gmail.com>
2019-08-26 00:52:20 -0700

    Initial Commit

---
+++ ProfilePhotoUploadView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProfilePhotoUploadView
+//
+//  Created by chris davis on 8/26/19.
+//  Copyright © 2019 chris davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
